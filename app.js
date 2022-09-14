d50e264256e93de37cb4669d4b2df15819f23e72
azrulwm <azrulazmi.wm@gmail.com>
2022-09-14 10:24:20 +0800

    finishing up scrolling experience

--- app.js
+++ app.js
@@ -19,21 +19,28 @@ links.forEach((link, i) => {
   });
 });
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    const projectImg = entry.target.querySelector(".project-img");
-    const projectContent = entry.target.querySelector(".project-content");
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      console.log(entries);
+      const projectImg = entry.target.querySelector(".project-img");
+      const projectContent = entry.target.querySelector(".project-content");
 
-    if (entry.isIntersecting) {
-      projectImg.classList.add("blur");
-      projectContent.classList.add("text-visible");
-      return; // if we added the class, exit the function
-    }
+      if (entry.isIntersecting) {
+        projectImg.classList.add("blur");
+        projectContent.classList.add("text-visible");
+        return; // if we added the class, exit the function
+      }
 
-    // We're not intersecting, so remove the class!
-    projectImg.classList.remove("blur");
-    projectContent.classList.remove("text-visible");
-  });
+      // We're not intersecting, so remove the class!
+      projectImg.classList.remove("blur");
+      projectContent.classList.remove("text-visible");
+    });
+  },
+  { threshold: 1 }
+);
+
+const projectCards = document.querySelectorAll(".project-card");
+projectCards.forEach((projectCard) => {
+  observer.observe(projectCard);
 });
-
-observer.observe(document.querySelector(".project-card"));
